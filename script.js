c6fe23911e68d66f1f663a1721c0ed2fdd65fc65
nora <usernamesaresoharddtomake@gmail.com>
2023-07-06 22:43:01 +0800

    update delete btn ui

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ let myLibrary = [
   {title: "The Alchemist", author: "Paulo Coelho", status: "Read"}
 ];
 
+class Book {
+  constructor(title, author, status) {
+  this.title = title;
+  this.author = author;
+  this.status = status;
+  }
+}
+
 const $title = document.querySelector("#name");
 const $author = document.querySelector("#author");
 const $status = document.querySelector("#status");
@@ -34,14 +42,6 @@ const $table = document.querySelector("table").addEventListener("click", (e) => 
   feather.replace()
 })
 
-class Book {
-  constructor(title, author, status) {
-  this.title = title;
-  this.author = author;
-  this.status = status;
-  }
-}
-
 function addBookToLibrary() {
   // ensure value is not empty 
   if ($title.value.length === 0 || $author.value.length === 0) {
@@ -64,6 +64,7 @@ function findBook(libraryArray, title) {
   if (libraryArray.length === 0 || libraryArray === null) {
     return;
   }
+
   for (book of libraryArray)
     if (book.title === title) {
       return libraryArray.indexOf(book);
@@ -71,7 +72,7 @@ function findBook(libraryArray, title) {
 }
 
 function removeBook(currentBook) {
-  myLibrary.splice(currentBook, currentBook + 1);
+  myLibrary.splice(currentBook, 1);
 }
 
 function changeStatus(book) {
@@ -89,20 +90,19 @@ function render() {
         <td class="py-4">${book.author}</td>
         <! -- toggle read -->
         <td class="py-4"><button class="status-btn border border-[#d1d1d1] rounded-md px-6 py-2">${book.status}</button></td>
-        <td><button class="del-btn">delete</button></td>
+        <! -- remove btn -->
+        <td class="static md:absolute top-5 right-5">
+          <button class="del-btn flex flex-row justify-center items-center gap-2 pl-5 pr-6 py-2 text-[#bebebe] hover:text-[#eee] dark:hover:text-[#232323] bg-[#232323] dark:bg-white rounded-md">
+            <i data-feather="trash-2" class="w-5 h-5  transition duration-300"></i>
+            <span class="">Delete</span>
+          </button>
+        </td>
       </tr>
       `;
     $tableBody.insertAdjacentHTML("beforeEnd", htmlBook);
   });
 }
-        // <! -- remove btn -->
-        // <td class="static md:absolute top-5 right-5">
-        //   <button class="px-8 py-2">
-        //     <span class="del-btn flex flex-row items-center">
-        //       <i data-feather="trash-2" class="w-5 h-5 text-[#bebebe] hover:text-[#232323] dark:hover:text-[#eee] transition duration-300"></i>
-        //     </span>
-        //   </button>
-        // </td>
+
 
 
 render();
